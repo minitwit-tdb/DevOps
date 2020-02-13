b4715508299bf399b60b6b7aedc5ec1334554b2c
Lasse Felskov Agersten <lfa@appear.dk>
2020-02-13 23:04:38 +0100

    allow application to run without docker if desired

--- src/database/getConnection.ts
+++ src/database/getConnection.ts
@@ -1,10 +1,11 @@
+import * as fallback from '../mariadb.json'
 import mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  host: process.env.MYSQL_HOST || fallback.host,
+  user: process.env.MYSQL_USER || fallback.user,
+  password: process.env.MYSQL_ROOT_PASSWORD || fallback.password,
+  database: process.env.MYSQL_DATABASE || fallback.database
 })
 
 export async function getConnection (): Promise<mariadb.Connection> {
