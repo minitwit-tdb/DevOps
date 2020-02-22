0e68fa8965cab0fe35fef44ec491a16285a832da
Lasse Felskov Agersten <lfa@appear.dk>
2020-02-22 14:24:02 +0100

    debug ci - fix getConnection hard dep on mariadb.json

--- src/database/getConnection.ts
+++ src/database/getConnection.ts
@@ -1,14 +1,30 @@
-import * as fallback from '../mariadb.json'
 import mariadb = require('mariadb');
 
-const pool = mariadb.createPool({
-  host: process.env.MYSQL_HOST || fallback.host,
-  user: process.env.MYSQL_USER || fallback.user,
-  password: process.env.MYSQL_ROOT_PASSWORD || fallback.password,
-  database: process.env.MYSQL_DATABASE || fallback.database
-})
+let pool: mariadb.Pool
 
 export async function getConnection (): Promise<mariadb.Connection> {
+  if (!pool) {
+    try {
+      const fallback = await require('../mariadb.json')
+
+      pool = mariadb.createPool({
+        host: process.env.MYSQL_HOST || fallback.host,
+        user: process.env.MYSQL_USER || fallback.user,
+        password: process.env.MYSQL_ROOT_PASSWORD || fallback.password,
+        database: process.env.MYSQL_DATABASE || fallback.database
+      })
+    } catch {
+      // In case we cannot find our fallback file, then simply attempt to login
+      // to mariaDB using environment variables.
+      pool = mariadb.createPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_ROOT_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+      })
+    }
+  }
+
   return pool.getConnection()
 }
 
