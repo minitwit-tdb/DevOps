ab7dd2c3b73870690387e0e3fb4dad4849f61979
miom <miom@itu.dk>
2020-03-25 23:46:29 +0100

    Fixed some errors

--- src/utils/handleUncaughtException.ts
+++ src/utils/handleUncaughtException.ts
@@ -3,11 +3,11 @@ import { logger } from '../utils'
 
 export async function handleUncaughtException (shutdown: () => Promise<void>, err: unknown): Promise<void> {
   if (err instanceof Error) {
-    logger.error('(Uncaught exception) ' + err.message + ' Stack:' + err.stack)
+    logger.error(`(Uncaught exception) ${err.message} Stack: ${err.stack}`)
     console.error('Uncaught exception: ', err.message)
     console.error(err.stack)
   } else {
-    logger.error('(Uncaught exception) ' + err)
+    logger.error(`Uncaught exception: ${err}`)
     console.error('Uncaught exception: ', err)
   }
 
