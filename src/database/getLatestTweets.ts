5e2810eb5c71f1ffde11b752ef6c117ba18367cc
Lasse Felskov Agersten <lfa@appear.dk>
2020-03-26 15:43:19 +0100

    fix: fixed performance regression when visiting frontpage

--- src/database/getLatestTweets.ts
+++ src/database/getLatestTweets.ts
@@ -1,13 +1,41 @@
 import { PER_PAGE } from '../config'
-import { Message, User } from '../models'
+import { getSequelize } from './getSequelize'
+import sequlize = require('sequelize')
 
-export async function getLatestTweets (limit: number = PER_PAGE): Promise<Message[]> {
-  return Message.findAll({
-    order: [['pub_date', 'DESC']],
-    limit,
-    where: {
-      flagged: false
-    },
-    include: [{ model: User, as: 'User' }]
-  })
+interface RawMessage {
+  message_id: string
+  author_id: string
+  text: string
+  pub_date: string
+  flagged: boolean
+  User: {
+    user_id: string
+    username: string
+    email: string
+  }
+}
+
+export async function getLatestTweets (limit: number = PER_PAGE): Promise<RawMessage[]> {
+  const sq = await getSequelize()
+
+  return sq.query({
+    query: `
+      SELECT Messages.*, Users.* from Messages
+        LEFT JOIN Users ON Messages.author_id = Users.user_id
+        ORDER BY Messages.message_id DESC 
+        LIMIT ?
+    `,
+    values: [limit]
+  }, { type: sequlize.QueryTypes.SELECT, raw: true }).map((message: any) => ({
+    message_id: message.message_id,
+    author_id: message.author_id,
+    text: message.text,
+    pub_date: message.pub_date,
+    flagged: message.flagged === 1,
+    User: {
+      user_id: message.user_id,
+      username: message.username,
+      email: message.email
+    }
+  }))
 }
